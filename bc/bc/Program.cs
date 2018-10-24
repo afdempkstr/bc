f9d5c31c1cac22e15aa109fa8b55f394152ec81f
Kostas Stroggylos <constantinos.stroggylos@peoplecert.onmicrosoft.com>
2018-10-24 22:00:15 +0300

    read input from user

--- bc/bc/Program.cs
+++ bc/bc/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace bc
 {
@@ -10,15 +6,47 @@ namespace bc
     {
         static void Main(string[] args)
         {
-            if ( args.Length == 0 )
+            int number = -1;
+            while (number < 0)
             {
-                Console.WriteLine("No number provided!");
-                return;
+                Console.WriteLine("Please give a number >= 0");
+                number = int.Parse(Console.ReadLine());
+            }
+ 
+            Console.WriteLine("The number you gave is " + number);
+
+            string result = Convert(number);
+            Console.WriteLine(result);
+
+            Console.WriteLine(Convert(2 * number));
+
+            Console.ReadKey();
+        }
+
+        static string Convert(int number)
+        {
+            int[] digits = new int[32];
+            int i = 0;
+
+            while (number / 2 >= 1)
+            {
+                int remainder = number % 2;
+                digits[i] = remainder;
+                number = number / 2;
+                i = i + 1;
             }
 
-            int number = 0;
-            number = int.Parse(args[0]);
+            digits[i] = number;
 
+            string result = "";
+
+            while (i >= 0)
+            {
+                result = result + digits[i];
+                i = i - 1;
+            }
+
+            return result;
         }
     }
 }
